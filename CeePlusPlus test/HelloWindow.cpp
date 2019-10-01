39ad495b7e3461e29473e4ed6f8820b20e90034a
SUVOLewis <lewis.kosztan@suvotechnology.com>
2019-10-01 15:45:03 +0100

    Got a rectangle to be drawn, can also toggle wireframe view on and off in code

--- CeePlusPlus test/HelloWindow.cpp
+++ CeePlusPlus test/HelloWindow.cpp
@@ -105,39 +105,46 @@ int main() {
 	glDeleteShader(fragmentShader);
 
 	// Triangle
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
+	//float vertices[] = {
+	//	-0.5f, -0.5f, 0.0f,
+	//	0.5f, -0.5f, 0.0f,
+	//	0.0f, 0.5f, 0.0f
+	//};
 
 	// Rectangle (two triangles)
-	//float vertices[] = {
-	//	// first triangle
-	//	 0.5f,  0.5f, 0.0f,  // top right
-	//	 0.5f, -0.5f, 0.0f,  // bottom right
-	//	-0.5f,  0.5f, 0.0f,  // top left 
-	//	// second triangle
-	//	 0.5f, -0.5f, 0.0f,  // bottom right
-	//	-0.5f, -0.5f, 0.0f,  // bottom left
-	//	-0.5f,  0.5f, 0.0f   // top left
-	//};
-	//unsigned int indices[] = {
-	//	0, 1, 3, // first triangle
-	//	1, 2, 3 // second triangle
-	//};
+	float vertices[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3 // second triangle
+	};
 
 	unsigned int VBO; // vertex buffer object
 	unsigned int VAO; // vertex array object
+	unsigned int EBO;
 
+	// Gen buffers and arrays
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// Bind buffers and arrays
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	// Uncomment to draw in wireframe mode
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	 
 	// RENDER LOOP, render the window as dark turquoise
 	while (!glfwWindowShouldClose(window)) {
@@ -151,7 +158,8 @@ int main() {
 		// Draw our triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	}
 
 	glfwTerminate();
